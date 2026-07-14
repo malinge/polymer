87c9aea84412ea5b637c12690fa9cf1e964c86f8
张西锋 <zhangxifeng@163.com>
2026-07-14 16:04:16 +0800

    优化

--- polymer-application/src/main/java/com/polymer/PolymerApplication.java
+++ polymer-application/src/main/java/com/polymer/PolymerApplication.java
@@ -3,6 +3,7 @@ package com.polymer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 import org.springframework.cache.annotation.EnableCaching;
 
 /**
@@ -13,6 +14,9 @@ import org.springframework.cache.annotation.EnableCaching;
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class PolymerApplication {
     public static void main(String[] args) {
+        // 生产环境强制禁用 DevTools 热重启，防止类加载器泄漏
+        System.setProperty("spring.devtools.restart.enabled", "false");
         SpringApplication.run(PolymerApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  polymer启动成功   ლ(´ڡ`ლ)ﾞ");
     }
 }
